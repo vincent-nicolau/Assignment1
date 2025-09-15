5b14a3543f5fe84da97682e3e83c02b2e360d2d9
Vincent Nicolau <ven00002@mix.wvu.edu>
2025-09-15 12:31:08 -0400

    Enter key now summarizes

--- Assignment1/Form1.Designer.cs
+++ Assignment1/Form1.Designer.cs
@@ -118,6 +118,7 @@
             // 
             // Assignment1
             // 
+            AcceptButton = btnSummarize;
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
